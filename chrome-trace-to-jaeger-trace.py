2ede9c26e373f01a4f5dbe477d5e6be8c87118b5
umaumax <umauma7083@gmail.com>
2024-03-23 16:36:49 +0900

    Add features

--- chrome-trace-to-jaeger-trace.py
+++ chrome-trace-to-jaeger-trace.py
@@ -2,10 +2,10 @@
 
 import json
 import argparse
-import uuid
+import time
 
 
-def normalize_chrome_trace(input_chrome_trace):
+def normalize_chrome_trace(input_chrome_trace, option):
     output_normalized_chrome_trace = input_chrome_trace.copy()
     normalized_trace_events = []
     for i, event in enumerate(input_chrome_trace['traceEvents']):
@@ -30,19 +30,27 @@ def normalize_chrome_trace(input_chrome_trace):
                 '[warning] There is no implementation of ".traceEvents[{}].ph": "{}"'.format(
                     i, event['ph']))
             continue
+
+    sort_function = (lambda x: x['ts'])
+    if option['broken-trace']:
+        sort_function = (
+            lambda x: (
+                x['ph'],
+                x['ts'] if x['ph'] == 'B' else -
+                x['ts']))
     normalized_trace_events = sorted(
         normalized_trace_events,
-        key=lambda x: x['ts'])
+        key=sort_function)
     output_normalized_chrome_trace['traceEvents'] = normalized_trace_events
     return output_normalized_chrome_trace
 
 
 def convert_chrome_trace_to_jaeger(
-        input_chrome_trace_file, output_jaeger_trace_file):
+        input_chrome_trace_file, output_jaeger_trace_file, option):
     with open(input_chrome_trace_file, 'r') as f:
         chrome_trace_data = json.load(f)
 
-    chrome_trace_data = normalize_chrome_trace(chrome_trace_data)
+    chrome_trace_data = normalize_chrome_trace(chrome_trace_data, option)
     data = []
     process_map = {}
     for event in chrome_trace_data['traceEvents']:
@@ -51,9 +59,11 @@ def convert_chrome_trace_to_jaeger(
 
     span_index = 0
     processes = {}
+    spans = []
+    span_children_map = {}
+    trace_id = str(hex(int(time.time() * 1000)))[-7:]  # unixtimestamp[ms]
     for process_key, events in process_map.items():
         event_stack = []
-        spans = []
         for event in events:
             if event['ph'] == 'B':
                 span_id = "span:{}".format(str(span_index))
@@ -63,23 +73,31 @@ def convert_chrome_trace_to_jaeger(
             assert len(event_stack) > 0, 'len(evene_stack) must be more than 0'
             assert event['ph'] == 'E', '"ph" is expected to be "E": {}'.format(
                 event)
-            trace_id = str(uuid.uuid1()).replace('-', '')
             (span_id, pre_event) = event_stack[-1]
-            assert event['name'] == pre_event['name'], 'pre_event["name"] must be {}: {}'.format(
-                event['name'], pre_event)
+            if not option['broken-trace']:
+                assert event['name'] == pre_event['name'], 'pre_event["name"] must be {}: {}'.format(
+                    event['name'], pre_event)
             event_stack.pop()
             startTime = pre_event['ts']
             endTime = event['ts']
             duration = endTime - startTime
+
             references = []
-            if len(event_stack) > 0:
-                (parent_span_id, _) = event_stack[-1]
-                reference = {
-                    "refType": "CHILD_OF",
-                    "traceID": trace_id,
-                    "spanID": parent_span_id,
-                }
-                references = [reference]
+            childSpanIds = []
+            if not option['without-hierarchy']:
+                if len(event_stack) > 0:
+                    (parent_span_id, _) = event_stack[-1]
+                    reference = {
+                        "refType": "CHILD_OF",
+                        "traceID": trace_id,
+                        "spanID": parent_span_id,
+                    }
+                    references = [reference]
+                    span_children_map.setdefault(
+                        parent_span_id, []).append(span_id)
+                if span_id in span_children_map:
+                    childSpanIds = span_children_map[span_id]
+            hasChildren = len(childSpanIds) > 0
             span_data = {
                 "traceID": trace_id,
                 "spanID": span_id,
@@ -88,16 +106,16 @@ def convert_chrome_trace_to_jaeger(
                 "startTime": startTime,  # us
                 "duration": duration,  # ms
                 "tags": [
-                    {
-                        "key": "int value",
-                        "type": "int64",
-                        "value": 1234
-                    },
-                    {
-                        "key": "string value",
-                        "type": "string",
-                        "value": "hello"
-                    }
+                    # {
+                    # "key": "int value",
+                    # "type": "int64",
+                    # "value": 1234
+                    # },
+                    # {
+                    # "key": "string value",
+                    # "type": "string",
+                    # "value": "hello"
+                    # }
                 ],
                 "logs": [],
                 "processID": process_key,
@@ -105,9 +123,8 @@ def convert_chrome_trace_to_jaeger(
                 "process": {},
                 "relativeStartTime": 0,
                 "depth": 0,
-                "hasChildren": False,
-                "childSpanIds": [
-                ]
+                "hasChildren": hasChildren,
+                "childSpanIds": childSpanIds,
             }
             spans.append(span_data)
         event = events[0]
@@ -146,13 +163,26 @@ def main():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-o', '--output-filepath', default='/dev/stdout')
     parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument(
+        '-b',
+        '--broken-trace',
+        action='store_true',
+        help='Input file trace is broken (parent-child relationship cannot be obtained correctly) (The input order is used as the parent-child relationship.)')
+    parser.add_argument(
+        '--without-hierarchy',
+        action='store_true',
+        help='Do not include parent-child relationship information in output files')
     parser.add_argument('input_filepath')
     parser.add_argument('args', nargs='*')
 
     args, extra_args = parser.parse_known_args()
+    option = {
+        'broken-trace': args.broken_trace,
+        'without-hierarchy': args.without_hierarchy,
+    }
     convert_chrome_trace_to_jaeger(
         args.input_filepath,
-        args.output_filepath)
+        args.output_filepath, option)
 
 
 if __name__ == '__main__':
